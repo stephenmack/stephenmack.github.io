7c0830fbd7645b0b8d26f1dea026a14777fe9be3
dannyskelley <danny@openhotel.com>
2018-06-15 20:21:05 -0400

    s

---
+++ header.js
@@ -0,0 +1,19 @@
+    document.write('\
+\
+   <header ID="home" class="header">
+  <a href="/" class="logo"><IMG SRC="images/logo.svg">Hoosi</a>
+  <input class="menu-btn" type="checkbox" id="menu-btn" />
+  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+  <ul class="menu">
+    <li><a href="#home">Home</a></li>
+    <li><a href="#services">Services</a></li>
+    <li><a href="#work">Our Work</a></li>
+    <li><a href="#quoteForm">Free Quote</a></li>
+  </ul>
+</header>\
+\
+     <p>Notice however, that you have to escape LF's with a '\', just like\
+        demonstrated in this code listing.\
+    </p>\
+\
+');
