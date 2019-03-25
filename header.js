397fbfc7813aca15358662fdfde486a5cd629d6f
dannyskelley <danny@openhotel.com>
2019-03-25 16:07:23 -0400

    a
    a

--- header.js
+++ header.js
@@ -1,16 +1,9 @@
     document.write('\
 \
-   <header ID="home" class="header">
-  <a href="/" class="logo"><IMG SRC="images/logo.svg">Hoosi</a>
-  <input class="menu-btn" type="checkbox" id="menu-btn" />
-  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-  <ul class="menu">
-    <li><a href="#home">Home</a></li>
-    <li><a href="#services">Services</a></li>
-    <li><a href="#work">Our Work</a></li>
-    <li><a href="#quoteForm">Free Quote</a></li>
-  </ul>
-</header>\
+        <DIV CLASS="stickyHead">
+       <A ID="email" HREf="mailto:stephenmack@bellsouth.net">stephenmack@bellsouth.net</A><SPAN CLASS="pipe">|</SPAN>
+      <A ID="phone" HREF="tel:3867475367">3867475367</A>
+    </DIV>  \
 \
      <p>Notice however, that you have to escape LF's with a '\', just like\
         demonstrated in this code listing.\
